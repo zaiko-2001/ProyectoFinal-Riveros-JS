29455fae9b165cc8f1782c3e385ce9d2a34ba064
Paulo Riveros <paulo.riveros2001@gmail.com>
2024-10-06 12:39:39 -0300

    primer commit

---
+++ js/productos.js
@@ -0,0 +1,46 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Set de Café Rústico",
+      descripcion: "Disfruta de un momento de tranquilidad con nuestro Set de Café Rústico.",
+      precio: 10000,
+      imagen: "imagenes/taza 1.jpeg",
+    },
+    {
+      id: 2,
+      nombre: "Taza Minimalista Floreada",
+      descripcion: "Disfruta de tus bebidas favoritas con estilo gracias a nuestra Taza Minimalista Floreada.",
+      precio: 5000,
+      imagen: "imagenes/taza 2.jpeg",
+    },
+    {
+      id: 3,
+      nombre: "Taza Gatito Encantador",
+      descripcion: "Añade un toque de ternura a tu rutina diaria con nuestra Taza Gatito Encantador.",
+      precio: 6000,
+      imagen: "imagenes/taza 3.jpeg",
+    },
+    {
+      id: 4,
+      nombre: "Taza Sinfonía Gatuna",
+      descripcion: "Esta taza celebra la melodía de la creatividad con gatos tocando instrumentos musicales.",
+      precio: 5000,
+      imagen: "imagenes/taza 4.png",
+    },
+    {
+      id: 5,
+      nombre: "Taza Partida Canina",
+      descripcion: "Esta taza captura la diversión y originalidad de una partida de póker entre perros caricaturizados.",
+      precio: 5700,
+      imagen: "imagenes/taza 5.png",
+    },
+    {
+      id: 6,
+      nombre: "Taza el Universo en tus Manos",
+      descripcion: "Esta taza te lleva en un viaje por las estrellas con su impresionante diseño de galaxia.",
+      precio: 5500,
+      imagen: "imagenes/taza 6.png",
+    },
+  ];
+
+  
---
+++ js/script.js
@@ -0,0 +1,65 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const botonesCarrito = document.querySelectorAll(".agregar-carrito");
+const botonVaciarCarrito = document.getElementById("vaciar-carrito");
+
+botonesCarrito.forEach((boton) => {
+  boton.addEventListener("click", agregarAlCarrito);
+});
+
+function agregarAlCarrito(event) {
+  const { id: productoId, nombre: productoNombre, precio: productoPrecio } = event.target.dataset;
+
+  const producto = {
+    id: productoId,
+    nombre: productoNombre,
+    precio: parseInt(productoPrecio),
+    cantidad: 1,
+  };
+
+  const productoExistente = carrito.find((item) => item.id === productoId);
+
+  productoExistente
+    ? productoExistente.cantidad++
+    : carrito.push({ ...producto });
+
+  actualizarCarrito();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function actualizarCarrito() {
+  const carritoItems = document.getElementById("carrito-items");
+  const carritoTotal = document.getElementById("carrito-total");
+
+  carritoItems.innerHTML = "";
+
+  let total = 0;
+
+  carrito.forEach(({ nombre, cantidad, precio }) => {
+    const productoElement = document.createElement("div");
+    productoElement.classList.add("d-flex", "justify-content-between", "mb-2");
+    productoElement.innerHTML = 
+    `<span>${nombre} <strong>(${cantidad}un.)</strong></span>` + 
+    `<span><strong>${(precio * cantidad).toFixed(2)}</strong></span>`;
+    carritoItems.appendChild(productoElement);
+
+    total += precio * cantidad;
+  });
+
+  if (!carrito.length) {
+    carritoItems.innerHTML = '<p class="text-muted">Tu carrito está vacío.</p>';
+  }
+
+  carritoTotal.textContent = total.toFixed(2);
+}
+
+
+function vaciarCarrito() {
+  carrito = []; 
+  localStorage.removeItem("carrito"); 
+  actualizarCarrito(); 
+}
+
+botonVaciarCarrito.addEventListener("click", vaciarCarrito);
+
+document.addEventListener("DOMContentLoaded", actualizarCarrito);
